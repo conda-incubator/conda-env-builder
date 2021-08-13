58c5c6fe32f5d7f6bd60af6abb4bac0d87cfc8fd
Nils Homer <nilshomer@gmail.com>
2021-08-13 16:35:52 -0700

    [internal] improve coverage on SpecTest

--- tools/test/src/com/github/nh13/condaenvbuilder/api/SpecTest.scala
+++ tools/test/src/com/github/nh13/condaenvbuilder/api/SpecTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.OptionValues
 
 class SpecTest extends UnitSpec with OptionValues {
 
-  "Spec" should "only apply defaults if no package was specified in the case of inheritance" in {
+  "Spec.compile" should "only apply defaults if no package was specified in the case of inheritance" in {
     // Defaults has a default version for foo, parent does not, and child has a different version than the default.
     // So when compiled, parent should have version 1.1 and child 1.2 for foo.
 
@@ -37,5 +37,63 @@ class SpecTest extends UnitSpec with OptionValues {
     // Compiled child should not have the default, which has been applied to the parent, applied to itself
     val compiledChild = environments.find(_.name == "child").value
     compiledChild.steps should contain theSameElementsInOrderAs Seq(CondaStep(requirements=Seq("foo=1.2").reqs))
+  }
+
+  it should "throw an exception when it detects a cyclical dependency in the environment inheritance" in {
+    // simple cycle
+    {
+      val parent = EnvironmentSpec(inherits = Seq("child"), environment = Environment(
+        name  = "parent",
+        steps = Seq(CondaStep(requirements = Seq("foo").reqs)),
+        group = "",
+      ))
+
+      val child = EnvironmentSpec(inherits = Seq("parent"), environment = Environment(
+        name  = "child",
+        steps = Seq(CondaStep(requirements = Seq("foo=1.2").reqs)),
+        group = ""
+      ))
+
+      val ex = intercept[IllegalArgumentException] {
+        Spec.compile(
+          specs        = Seq(parent, child),
+          defaults     = Seq.empty,
+          environments = Seq.empty
+        )
+      }
+
+      ex.getMessage should include ("Found a cyclical dependency in the environment inheritance")
+    }
+
+    // three-step cycle
+    {
+      val parent = EnvironmentSpec(inherits = Seq("grandchild"), environment = Environment(
+        name  = "parent",
+        steps = Seq(CondaStep(requirements = Seq("foo").reqs)),
+        group = "",
+      ))
+
+      val child = EnvironmentSpec(inherits = Seq("parent"), environment = Environment(
+        name  = "child",
+        steps = Seq(CondaStep(requirements = Seq("foo=1.2").reqs)),
+        group = ""
+      ))
+
+      val grandchild = EnvironmentSpec(inherits = Seq("child"), environment = Environment(
+        name  = "grandchild",
+        steps = Seq(CondaStep(requirements = Seq("foo=1.2").reqs)),
+        group = ""
+      ))
+
+      val ex = intercept[IllegalArgumentException] {
+        Spec.compile(
+          specs        = Seq(parent, child, grandchild),
+          defaults     = Seq.empty,
+          environments = Seq.empty
+        )
+      }
+
+      ex.getMessage should include ("Found a cyclical dependency in the environment inheritance")
+    }
   }
 }
